ebb8643b6051dadfaa578086cb4ea8cbd99cee4e
CodingEzio <assassinste@gmail.com>
2022-05-04 08:38:13 +0800

    Adding the order info plus containers for Tacos

---
+++ src/main/java/com/springinaction/tacocloud/TacoOrder.java
@@ -0,0 +1,25 @@
+package com.springinaction.tacocloud;
+
+import java.util.List;
+import java.util.ArrayList;
+import lombok.Data;
+
+@Data
+public class TacoOrder {
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccExpiration;
+    private String ccCVV;
+
+    // An order could contain multiple tacos
+    private List<Taco> tacos = new ArrayList<>();
+
+    // An order is dynamic
+    // You could add more tacos anytime
+    private void addTaco (Taco taco) {
+        this.tacos.add(taco);
+    }
+}
