da2b07e348fad2d078867b12ac0b9b44ec2a6ae5
CodingEzio <assassinste@gmail.com>
2022-05-27 20:19:08 +0800

    Put designed taco into the order, ready to deliver

---
+++ src/main/java/com/springinaction/tacocloud/web/OrderController.java
@@ -0,0 +1,24 @@
+package com.springinaction.tacocloud.web;
+
+import com.springinaction.tacocloud.TacoOrder;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+}
