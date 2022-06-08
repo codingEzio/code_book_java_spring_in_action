8a3ec896524d201743dc5371731dcbfe21391a47
CodingEzio <assassinste@gmail.com>
2022-06-08 20:16:23 +0800

    Replaced hardcoded values with database queries (manipulate as Java methods)

--- src/main/java/com/springinaction/tacocloud/web/IngredientByIdConverter.java
+++ src/main/java/com/springinaction/tacocloud/web/IngredientByIdConverter.java
@@ -5,30 +5,23 @@ import java.util.Map;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.springinaction.tacocloud.Ingredient;
 import com.springinaction.tacocloud.Ingredient.Type;
+import com.springinaction.tacocloud.data.IngredientRepository;
 
 @Component
 public class IngredientByIdConverter implements Converter<String, Ingredient> {
 
-	private Map<String, Ingredient> ingreMap = new HashMap<>();
+	private IngredientRepository ingredientRepo;
 
-	public IngredientByIdConverter() {
-		ingreMap.put("FLTO", new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
-		ingreMap.put("COTO", new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
-		ingreMap.put("GRBF", new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
-		ingreMap.put("CARN", new Ingredient("CARN", "Carnitas", Type.PROTEIN));
-		ingreMap.put("TMTO", new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
-		ingreMap.put("LETC", new Ingredient("LETC", "Lettuce", Type.VEGGIES));
-		ingreMap.put("CHED", new Ingredient("CHED", "Cheddar", Type.CHEESE));
-		ingreMap.put("JACK", new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
-		ingreMap.put("SLSA", new Ingredient("SLSA", "Salsa", Type.SAUCE));
-		ingreMap.put("SRCR", new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+	public IngredientByIdConverter (IngredientRepository ingredientRepo) {
+		this.ingredientRepo = ingredientRepo;
 	}
 
 	@Override
 	public Ingredient convert(String id) {
-		return ingreMap.get(id);
+		return ingredientRepo.findById(id).orElse(null);
 	}
 }
