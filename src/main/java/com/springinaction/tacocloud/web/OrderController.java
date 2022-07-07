c682585815ea8a8874e910e72f33879faccafc07
!Mensch <assassinste@gmail.com>
2022-07-08 05:31:44 +0800

    Injecting orderRepository into controllers to actually save orders

--- src/main/java/com/springinaction/tacocloud/web/OrderController.java
+++ src/main/java/com/springinaction/tacocloud/web/OrderController.java
@@ -3,23 +3,29 @@ package com.springinaction.tacocloud.web;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.validation.Errors;
 
 import lombok.extern.slf4j.Slf4j;
 
 import com.springinaction.tacocloud.TacoOrder;
-
+import com.springinaction.tacocloud.data.OrderRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+
+    private OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
 
     @GetMapping("/current")
     public String orderForm() {
@@ -28,16 +34,15 @@ public class OrderController {
 
     @PostMapping
     public String processOrder(@Valid TacoOrder order,
-                               Errors errors,
-                               SessionStatus sessionStatus) {
-        if (errors.hasErrors()){
+            Errors errors,
+            SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
             log.error("Error occurred when submitting orders.");
 
             return "orderForm";
         }
 
-        log.info("Order submitted: {}", order);
-
+        orderRepository.save(order);
         sessionStatus.setComplete();
 
         return "redirect:/";
