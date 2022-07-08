bb2d305ba6aa3f0f81c33ff7fae7c9ab6a677cea
!Mensch <assassinste@gmail.com>
2022-07-08 19:08:41 +0800

    Adding Spring Data JDBC annotations to be mapped

--- src/main/java/com/springinaction/tacocloud/Ingredient.java
+++ src/main/java/com/springinaction/tacocloud/Ingredient.java
@@ -1,15 +1,23 @@
 package com.springinaction.tacocloud;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Table;
+
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.AccessLevel;
 
 @Data
+@Table
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class Ingredient {
+
+    @Id
     private final String id;
+
     private final String name;
     private final Type type;
 
